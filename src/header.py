8bf9689b1e142fbb8412a0bd63d2b67803c536bf
zekmaro <zekmaro@yandex.ru>
2025-07-28 11:46:18 +0200

    feat: find CORR_GROUPS

--- src/header.py
+++ src/header.py
@@ -14,3 +14,19 @@ LABEL_MAPPING = {
 	"M": 1,  # Malignant
 	"B": 0   # Benign
 }
+
+CORR_GROUPS = [
+    ["radius_mean", "perimeter_mean", "area_mean"],
+    ['radius_se', 'perimeter_se', 'area_se'],
+    ['compactness_mean', 'concavity_mean', 'compactness_worst'],
+    ['smoothness_mean', 'smoothness_worst'],
+    ['texture_mean', 'texture_worst'],
+    ['fractal_dimension_mean', 'fractal_dimension_worst'],
+    ['concave_points_mean', 'concave_points_worst'],
+    ['symmetry_mean', 'symmetry_worst']
+]
+
+MEAN_FEATURES = [
+    "radius_mean", "texture_mean", "perimeter_mean", "area_mean", "smoothness_mean",
+    "compactness_mean", "concavity_mean", "concave_points_mean", "symmetry_mean", "fractal_dimension_mean"
+]
